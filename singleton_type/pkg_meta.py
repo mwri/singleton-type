16ed0fd7f83e8c1065f567c099bd86283c7777bb
Michael Wright <mjw@methodanalysis.com>
2022-04-07 23:04:31 +0100

    release 0.0.3

--- singleton_type/pkg_meta.py
+++ singleton_type/pkg_meta.py
@@ -3,7 +3,7 @@
 import sys
 
 name = "singleton-type"
-version = "0.0.2"
+version = "0.0.3"
 author = "Michael Wright"
 author_email = "mjw@methodanalysis.com"
 description = "Singleton meta class implementation"
